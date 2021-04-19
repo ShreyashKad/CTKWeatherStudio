051678d323c22a993a3ac9cdc3429c6e4c5d8b85
ShreyashKad <shreyashkad39@gmail.com>
2021-04-19 19:47:19 +0200

    Fixed a bug of need to call api twice by implementing useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import SearchCity from './components/SearchCityBar';
 import './css/App.css';
 
@@ -6,27 +6,41 @@ import './css/App.css';
 
 function App() {
 
-  const weatherApiKey = process.env.REACT_APP_OPENWEATHERMAPS_API_KEY;
+  // const weatherApiKey = process.env.REACT_APP_OPENWEATHERMAPS_API_KEY;
   
   
   let [apiData, setApiData] = useState({});
   let [city, setCity] = useState('');
+  let [usrInput, setUsrInput] = useState('');
   
-  // Function to store the city inserted by user
-  const handleInputChange = (value) => {
-    setCity(value.target.value)
+  /* 
+  Function to store the user input inserted by user
+  Input: city name (text)
+  */
+  const handleInputChange = (e) => {
+    setUsrInput(e.target.value)
+  }
+
+  const handleSubmit = () => {
+    setCity(usrInput)
   }
 
   // Function to fetch response of OpenWeatherAPI 
-  const getForcast = () => {
+  useEffect(() => {
+    const weatherApiKey = process.env.REACT_APP_OPENWEATHERMAPS_API_KEY;
     const weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherApiKey}`;
+    
     fetch(weatherApiUrl)
     .then(res => res.json())
     .then((data) => setApiData(data));
+  }, [city]);
 
+  useEffect(() => {
     console.log(apiData)
-  }
+  }, [apiData])
   
+
+
   return (
     <div className="App">
 
@@ -37,7 +51,7 @@ function App() {
         name="s"
         onChange={handleInputChange}
       />
-      <button type="submit" onClick={getForcast}>-></button>
+      <button type="submit" onClick={handleSubmit}>-></button>
 
       <SearchCity />
 
